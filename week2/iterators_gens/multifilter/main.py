1de6034df5b616aa71d705808c2b2373e0930d0c
stasofob <64842680+stasofob@users.noreply.github.com>
2022-04-01 16:18:57 +0300

    добавлено решение multifilter

--- week2/iterators_gens/multifilter/main.py
+++ week2/iterators_gens/multifilter/main.py
@@ -1,3 +1,13 @@
+def count_posneg(arg, funcs):
+    pos, neg = 0, 0
+    for func in funcs:
+        if func(arg):
+            pos += 1
+        else:
+            neg += 1
+    return pos, neg
+
+
 class multifilter:
     def judge_half(pos, neg):
         return pos >= neg
@@ -18,34 +28,26 @@ class multifilter:
         self.funcs = funcs
         self.judge = judge
 
-    def _count_posneg(argument, *funcs):
-        pos = 0
-        neg = 0
-        for func in funcs:
-            if func(argument):
-                pos += 1
-            else:
-                neg += 1
-        return pos, neg
-
     def __iter__(self):
         for elem in self.iterable:
-            if self.judge(self._count_posneg(elem, self.funcs)):
+            pos, neg = count_posneg(elem, self.funcs)
+            if self.judge(pos, neg):
                 yield elem
+
 
 def mul2(x):
     return x % 2 == 0
 
+
 def mul3(x):
     return x % 3 == 0
+
 
 def mul5(x):
     return x % 5 == 0
 
 
-a = [i for i in range(31)] # [0, 1, 2, ... , 30]
+a = [i for i in range(31)]  # [0, 1, 2, ... , 30]
 
 print(list(multifilter(a, mul2, mul3, mul5)))
 # [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
-
-
