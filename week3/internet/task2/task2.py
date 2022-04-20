aaca914b5eb42f13c2e2b748588b3fb9088fd05c
stasofob <64842680+stasofob@users.noreply.github.com>
2022-04-20 17:35:32 +0300

    сделал с регулярками
    Но проблема в получении строки

--- week3/internet/task2/task2.py
+++ week3/internet/task2/task2.py
@@ -1,29 +1,44 @@
 import re
+import requests
 
-from bs4 import BeautifulSoup
+url_html = "http://pastebin.com/raw/7543p0ns"
+# file_html = requests.get(url_html).text
 
-file_html = """ <a href="http://stepic.org/courses">
-                <a href='https://stepic.org'>
-                <a href='http://neerc.ifmo.ru:1345'>
-                <a href="ftp://mail.ru/distib" >
-                <a href="ya.ru">
-                <a href="www.ya.ru">
-                <a href="../skip_relative_links">"""
-
-soup = BeautifulSoup(file_html, 'lxml')
-urls = soup.find_all('a', href=True)
-urls = [url['href'] for url in urls]
-
-pattern = r'(^../)?(\w+://)?([\w.]+)'
+file_html = """<a href="http://stepic.org/courses">
+<a href="ftp://www.mya.ru">
+<a href='https://stepic.org'>
+<a link href='http://neerc.ifmo.ru:1345'>
+<a target="blank" href='http://sasd.ifmo.ru:1345'>
+<a href='http://neerc.ifmo.ru:1345'>
+<a href="../some_path/index.html">
+<a href="https://www.ya.ru">
+<a href="ftp://mail.ru/distib" >
+<a href="bya.ru">
+<a href="http://www.ya.ru">
+<a href="www.kya.ru">
+<a href="../skip_relative_links">
+<a href="http://stepic.org/courses">
+<a class = "hello" href= "http://ftepic.org/courses" id="dfdf">
+<p class = "hello" href= "http://dtepic.org/courses">
+<a class = "hello" href = "http://a.b.vc.ttepic.org/courses">
+<a href='https://stepic.org'>
+<a href='http://neerc.ifmo.ru:1345'>
+<a href = "ftp://mail.ru/distib" >
+<a href= "ya.ru">
+<a href ="www.ya.ru">
+<a href="../skip_relative_links">
+<link rel="image_src" href="https://examaple.org/files/6a2/72d/e09/6a272de0944f447fb5972c44cc02f795.png" />
+<a href="http://www.gtu.edu.ge/index_e.htm" target="_top">Georgian Technical University</a>﻿"""
+print(file_html)
+pattern = r'(<a href ?= ?[\'\"])(?!../)(\w+://)?([\w.]+)'
 urls_set = set()
 
-for url in urls:
-    match = re.search(pattern, url)
-    if not match.group(1):
-        urls_set.add(match.group(3))
-
-print(urls_set)
-urls = sorted(list(urls_set))
+urls = re.findall(pattern, file_html)
 print(urls)
+for url in urls:
+    urls_set.add(url[2])
 
+urls = sorted(list(urls_set))
 
+for url in urls:
+    print(url)
