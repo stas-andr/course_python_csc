799bf6dc95ab4c98417c5b11f9846dba9941b47a
stasofob <64842680+stasofob@users.noreply.github.com>
2022-04-21 15:58:27 +0300

    решена csv

--- week3/сsv_json/csv/main.py
+++ week3/сsv_json/csv/main.py
@@ -1,14 +1,14 @@
 import csv
 from datetime import datetime
 
-str_with_2015 = []
+primary_types_in_2015 = []
 with open("Crimes.csv", 'r') as input:
     reader = csv.DictReader(input)
     for row in reader:
         date = datetime.strptime(row['Date'], '%m/%d/%Y %H:%M:%S %p')
         year = date.year
         if year == 2015:
-            str_with_2015.append(row)
+            primary_types_in_2015.append(row['Primary Type'])
 
-max(count(row['Primary type']))
+print(max(primary_types_in_2015, key=primary_types_in_2015.count))
 
